43f716719d74f466da444cffd0a0ba6800f305d4
dot <01178395@pw.edu.pl>
2024-12-30 09:47:01 +0100

    Project folders

---
+++ src/gui/__init__.py
@@ -0,0 +1,6 @@
+from widgets import DirectoryTreeContainer, ImageContainer, ParamsContainer
+
+
+class App:
+    def __init__(self):
+        self.main_container = [DirectoryTreeContainer, ImageContainer, ParamsContainer]
